4e2a253a1f8b54a85556cfa2ae86492d20290602
Tony Pai <tony77794@gmail.com>
2018-07-09 12:37:59 +0800

    Support localhost ssl
    Install mkcert and include certificate in nginx config

--- start.sh
+++ start.sh
@@ -8,14 +8,34 @@ if lsof -Pi :80 -sTCP:LISTEN -t >/dev/null; then
   exit 1
 fi
 
+if [[ "$(uname)" -ne "Darwin" ]]; then
+  echo Only support MacOS for now.
+  exit 1
+fi
+
+if ! type -p mkcert &>/dev/null; then
+  echo Install mkcert...
+  brew install mkcert nss
+fi
+
+if type -p mkcert &>/dev/null; then
+  echo Create ssl certificate...
+  mkcert -install
+  mkcert localhost 127.0.0.1 ::1
+  mv localhost+2.pem cert.pem
+  mv localhost+2-key.pem cert-key.pem
+fi
+
 if type -p docker &>/dev/null; then
   echo Starting nginx...
   docker run -d \
     -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf \
+    -v $(pwd)/cert.pem:/root/cert.pem \
+    -v $(pwd)/cert-key.pem:/root/cert-key.pem \
     -v $(pwd)/access.log:/root/access.log \
-    -p 80:80 \
+    -p 443:443 \
     nginx
-  echo Done
+  echo Done.
 else
   echo "Docker is not installed."
 fi
